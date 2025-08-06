f013a8d9fbabafc1ced298b6838b157675106ad7
Chinmay Kanfade <chinmaykanfade2006@gmail.com>
2025-08-06 10:04:37 +0530

    added delete feature

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("script is connected");
+
+ function addTask(){
+let task=document.getElementById("taskInput").value;
+let li=document.createElement("li");
+ let button=document.createElement("button");
+ button.textContent="❌";
+button.onclick=function(){
+    li.remove();
+}
+let textNode = document.createTextNode(task);
+li.appendChild(textNode);
+li.appendChild(button);  // add button next to the text
+
+
+ 
+document.getElementById("taskList").appendChild(li);
+document.getElementById("taskInput").value="";
+}
