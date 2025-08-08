d11ff4ca18a2e04d39bba653ebdbbd90060fab55
Chinmay Kanfade <chinmaykanfade2006@gmail.com>
2025-08-08 21:32:51 +0530

    Updated css

--- script.js
+++ script.js
@@ -3,24 +3,30 @@ console.log("script is connected");
  function addTask(){
 let task=document.getElementById("taskInput").value;
 let li=document.createElement("li");
- let button=document.createElement("button");
- button.textContent="❌";
-button.onclick=function(){
+ let deletebutton=document.createElement("button");
+ deletebutton.textContent="❌";
+ deletebutton.classList.add("del-btn");
+ 
+deletebutton.onclick=function(){
     li.remove();
 }
 let donebutton=document.createElement("button");
 donebutton.textContent="✅";
+donebutton.classList.add("done-btn");
 donebutton.onclick=function(){
-    li.textContent.style.textDecoration="line-through";
-    li.textContent.style.opacity="0.6"
+    li.style.textDecoration="line-through";
+    li.style.opacity="0.6"
 }
 
 let textNode = document.createTextNode(task);
+let taskitem=document.createElement("div");
+taskitem.classList.add("taskitem");
 li.appendChild(textNode);
-li.appendChild(button);  
+li.appendChild(deletebutton);  
 li.appendChild(donebutton);
+li.appendChild(taskitem);
 
-
+donebutton.innerHTML="<span class='tick'>✅</span>"
  
 document.getElementById("taskList").appendChild(li);
 document.getElementById("taskInput").value="";
