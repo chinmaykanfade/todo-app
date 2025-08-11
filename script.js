fba5927058f7355c0b772e8eef7036d45cc73295
Chinmay Kanfade <chinmaykanfade2006@gmail.com>
2025-08-11 23:24:38 +0530

    UI Enhancement

--- script.js
+++ script.js
@@ -3,6 +3,67 @@ console.log("script is connected");
  function addTask(){
 let task=document.getElementById("taskInput").value;
 let li=document.createElement("li");
+let editbtn=document.createElement("button");
+editbtn.classList.add("edit-btn");
+editbtn.textContent=" ✏️";
+
+
+editbtn.onclick=function(){
+   //back-up use //let newtask=prompt("Edit your List",li.firstChild.textContent);
+
+    //if(newtask!=null&& newtask.trim()!=""){
+        //li.firstChild.textContent=newtask;
+    //}
+    let originalTextNode=li.firstChild;
+    if(!originalTextNode)return;
+
+    const backup=originalTextNode.cloneNode(true);
+
+    const currentText=originalTextNode.textContent.trim();
+    const input=document.createElement("input");
+    input.type=textNode;
+    input.className="edit-input";
+    input.value=currentText;
+
+    li.replaceChild(input,originalTextNode);
+    input.focus();
+    input.setSelectionRange(input.value.length,input.value.length);
+
+    let cancelled=false;
+
+    function saveEdit(){
+        if(cancelled)return;
+        const newText=input.value.trim();
+        if(newText.length>0){
+            const newTextNode=document.createTextNode(newText +"");
+            li.replaceChild(newTextNode,input);
+        }
+        else{
+            li.replaceChild(backup,input);
+        }
+        }
+
+        function cancelEdit(){
+            cancelled=true;
+            li.replaceChild(backup,input);
+        }
+
+        input.onkeydown=function(e){
+            if(e.key=="Enter")saveEdit();
+            else if(e.key=="Escape")cancelEdit();
+        };
+
+        input.onblur=function(){
+            setTimeout(()=>{
+                if(!cancelled) saveEdit();
+            },100);
+        };
+    }
+
+
+
+
+
  let deletebutton=document.createElement("button");
  deletebutton.textContent="❌";
  deletebutton.classList.add("del-btn");
@@ -14,20 +75,20 @@ let donebutton=document.createElement("button");
 donebutton.textContent="✅";
 donebutton.classList.add("done-btn");
 donebutton.onclick=function(){
-    li.style.textDecoration="line-through";
-    li.style.opacity="0.6"
-}
+    li.classList.toggle("done");
+};
 
 let textNode = document.createTextNode(task);
 let taskitem=document.createElement("div");
 taskitem.classList.add("taskitem");
 li.appendChild(textNode);
-li.appendChild(deletebutton);  
-li.appendChild(donebutton);
+taskitem.appendChild(deletebutton);  
+taskitem.appendChild(donebutton);
+taskitem.appendChild(editbtn);
 li.appendChild(taskitem);
 
 donebutton.innerHTML="<span class='tick'>✅</span>"
  
 document.getElementById("taskList").appendChild(li);
 document.getElementById("taskInput").value="";
-}
+} 
